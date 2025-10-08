9af17e501967d5d63381b8296da9782eb41bbf3c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-10-08 12:50:59 +0200

    Build placeholder signals for post & delete (app_videos)

--- app_videos/models.py
+++ app_videos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=500)
+    video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- app_videos/signals.py
+++ app_videos/signals.py
@@ -1,6 +1,7 @@
+import os
 from .models import Video
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 
 
 @receiver(post_save, sender=Video)
@@ -9,6 +10,15 @@ def video_post_save(sender, instance, created, **kwargs):
     if created:
         print('New video created')
 
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Video` object is deleted.
+    """
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
 
 
 # post_save after saving
