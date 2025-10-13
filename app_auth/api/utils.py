4d6726c6d48447af394f1f26c410814aebf16863
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-10-13 14:07:47 +0200

    Fix issue: attached app logo not rendering (confirmation mail)

--- app_auth/api/utils.py
+++ app_auth/api/utils.py
@@ -1,18 +1,24 @@
+from django.conf import settings
 import os
+from email.mime.image import MIMEImage
 from django.core.mail import EmailMultiAlternatives
+from email.mime.image import MIMEImage
 from django.template.loader import render_to_string
 
 from dotenv import load_dotenv
 load_dotenv()
 
 def send_activation_email(user, activation_link):
+
     from_email = os.getenv("DEFAULT_FROM_EMAIL")
-    site_url = os.getenv("FRONTEND_URL", "http://localhost:8000") 
+    site_url = os.getenv("FRONTEND_URL", "http://localhost:8000")
+    image_path = os.path.join(settings.BASE_DIR, 'app_auth', 'static', 'app_auth', 'logo_icon.png')
+
     context = {
         'user': user,
         'activation_link': activation_link,
         'site_url': site_url,
-        'logo_url': f"{site_url}/media/templates/logo_icon.svg",
+        'logo_cid': 'logo_cid',
     }
     text_content = render_to_string(
         'confirm_account.txt', context)
@@ -24,11 +30,16 @@ def send_activation_email(user, activation_link):
         text_content,
         from_email,
         [user.email],
-        fail_silently=False,
     )
 
     msg.attach_alternative(html_content, "text/html")
-    msg.send()
+
+    with open(image_path, 'rb') as file:
+        img = MIMEImage(file.read())
+        img.add_header('Content-ID', '<logo_cid>')
+        img.add_header('Content-Disposition', 'inline', filename='logo_icon.png')
+        msg.attach(img)
+    msg.send(fail_silently=False)
 
 def send_password_reset_email(user, password_reset_link):
     print("Email for password reset was sent")
