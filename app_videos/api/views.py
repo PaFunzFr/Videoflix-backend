0b3af244eb8e6889a553d252f7ec0ef4ae4ad79f
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-10-14 16:05:22 +0200

    Add permission/auth to function based views(serve_hls)

--- app_videos/api/views.py
+++ app_videos/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from rest_framework import generics
 from app_videos.models import Video
 from app_auth.api.views import CookieJWTAuthentication
@@ -6,6 +7,18 @@ from .serializers import VideoListSerializer
 from pathlib import Path
 from django.http import FileResponse, Http404
 from django.conf import settings
+
+
+def auth_permission_check(request):
+    # Authentication
+    user_auth = CookieJWTAuthentication().authenticate(request)
+    if user_auth is None:
+        raise PermissionDenied("Authentication credentials were not provided")
+
+    # Permission Check
+    if not IsAuthenticated().has_permission(request, None):
+        raise PermissionDenied("User not allowed")
+
 
 class VideoListView(generics.ListAPIView):
     authentication_classes = [CookieJWTAuthentication]
@@ -15,6 +28,7 @@ class VideoListView(generics.ListAPIView):
 
 
 def serve_hls_playlist(request, pk, resolution):
+    auth_permission_check(request)
     file_path = Path(settings.MEDIA_ROOT) / f"video/{pk}/{resolution}/index.m3u8"
     if not file_path.exists():
         raise Http404("HLS playlist not found.")
@@ -22,6 +36,7 @@ def serve_hls_playlist(request, pk, resolution):
 
 
 def serve_hls_segment(request, pk, resolution, segment):
+    auth_permission_check(request)
     file_path = Path(settings.MEDIA_ROOT) / f"video/{pk}/{resolution}/{segment}"
     if not file_path.exists():
         raise Http404("Segment not found.")
