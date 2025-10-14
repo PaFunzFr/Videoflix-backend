7b1e97324653b096f2e3f29044ac58f79ff5afa2
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-10-14 08:54:35 +0200

    Adjust Video model (add attributes)

--- app_videos/admin.py
+++ app_videos/admin.py
@@ -1,3 +1,18 @@
+from django import forms
 from django.contrib import admin
+from .models import Video, CATEGORY_CHOICES
 
-# Register your models here.
+
+class VideoAdminForm(forms.ModelForm):
+    category = forms.ChoiceField(
+        choices=[("", "Choose Category")] + CATEGORY_CHOICES,
+        required=True,
+    )
+
+    class Meta:
+        model = Video
+        fields = "__all__"
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    form = VideoAdminForm
--- app_videos/models.py
+++ app_videos/models.py
@@ -1,9 +1,36 @@
 from django.db import models
 
+CATEGORY_CHOICES = [
+    ("action", "Action"),
+    ("adventure", "Adventure"),
+    ("animation", "Animation"),
+    ("comedy", "Comedy"),
+    ("crime", "Crime"),
+    ("documentary", "Documentary"),
+    ("drama", "Drama"),
+    ("fantasy", "Fantasy"),
+    ("historical", "Historical"),
+    ("horror", "Horror"),
+    ("musical", "Musical"),
+    ("mystery", "Mystery"),
+    ("romance", "Romance"),
+    ("science_fiction", "Science Fiction"),
+    ("thriller", "Thriller"),
+    ("war", "War"),
+    ("western", "Western"),
+    ("biography", "Biography"),
+    ("family", "Family"),
+    ("sport", "Sport"),
+]
+
+
 class Video(models.Model):
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=500)
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, blank=False, null = False, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} | {self.description or 'Video Description'} | {self.category} | Uploaded: {self.created_at:%Y-%m-%d %H:%M}"
