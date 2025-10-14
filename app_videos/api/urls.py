6fdb1146447495a7b6eb5aa7fd7a0f0cf29a42a8
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-10-14 14:15:36 +0200

    Add endpoint for video streaming (FileResponse)

--- app_videos/api/urls.py
+++ app_videos/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import VideoListView
+from .views import VideoListView, serve_hls_playlist, serve_hls_segment
 
 urlpatterns = [
     path('video/', VideoListView.as_view(), name="video-list"),
+    path('video/<int:pk>/<str:resolution>/index.m3u8', serve_hls_playlist, name='serve_hls_playlist'),
+    path('video/<int:pk>/<str:resolution>/<str:segment>', serve_hls_segment, name='serve_hls_segment'),
 ]
 
--- app_videos/api/views.py
+++ app_videos/api/views.py
@@ -3,9 +3,26 @@ from rest_framework import generics
 from app_videos.models import Video
 from app_auth.api.views import CookieJWTAuthentication
 from .serializers import VideoListSerializer
+from pathlib import Path
+from django.http import FileResponse, Http404
+from django.conf import settings
 
 class VideoListView(generics.ListAPIView):
     authentication_classes = [CookieJWTAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = VideoListSerializer
     queryset = Video.objects.all()
+
+
+def serve_hls_playlist(request, pk, resolution):
+    file_path = Path(settings.MEDIA_ROOT) / f"video/{pk}/{resolution}/index.m3u8"
+    if not file_path.exists():
+        raise Http404("HLS playlist not found.")
+    return FileResponse(open(file_path, 'rb'), content_type='application/vnd.apple.mpegurl')
+
+
+def serve_hls_segment(request, pk, resolution, segment):
+    file_path = Path(settings.MEDIA_ROOT) / f"video/{pk}/{resolution}/{segment}"
+    if not file_path.exists():
+        raise Http404("Segment not found.")
+    return FileResponse(open(file_path, 'rb'), content_type='video/mp2t')
