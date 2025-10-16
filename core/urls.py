bdf3e65711e8eb5a90d81c86cc17a97916aff458
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-10-16 10:11:06 +0200

    Add docu paths (including swagger-io) to urlpatterns(core)

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
@@ -28,6 +34,11 @@ urlpatterns = [
         path('', include('app_auth.api.urls')),
         path('', include('app_videos.api.urls')),
     ])),
+
+    # API documentation (including Swagger UI)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
